15fd926fe9a1e71dab5358647d12425880dbfd08
Sander Vocke <sandervocke@gmail.com>
2024-05-07 11:11:20 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 readonly SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 echo "Attempting install to: /tmp/bin"
 
+mkdir -p /tmp/bin
+if [ "$?" != "0" ]; then
+   echo "Failed, retrying with sudo"
+   sudo mkdir -p /tmp/bin
+fi
+
 install -v -m755 "$SCRIPT_DIR/wrap-shell.sh" "/tmp/bin/wrap-shell.sh"
 if [ "$?" != "0" ]; then
    echo "Failed, retrying with sudo"
