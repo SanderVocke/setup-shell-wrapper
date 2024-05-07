5bd537f34ed53c7d331be64b92e6f69f477e4b56
Sander Vocke <sandervocke@gmail.com>
2024-05-07 10:22:47 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 readonly SCRIPT_PATH=$(readlink "$0")
 readonly SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-echo "Attempting install to: "/tmp/abin/wrap-shell.sh"
-mkdir -p "$pwd/abin"
+echo "Attempting install to: /tmp/abin/wrap-shell.sh"
+mkdir -p "/tmp/abin"
 install -v -m755 "$SCRIPT_DIR/wrap-shell.sh" "/tmp/abin/wrap-shell.sh"
 if [ "$?" != "0" ]; then
    echo "Failed, retrying with sudo"
