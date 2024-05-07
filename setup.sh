652a7eb89a56af6cd57e4b4bf3b705b8893fb3c9
Sander Vocke <sandervocke@gmail.com>
2024-05-07 10:43:36 +0200

    install to /bin

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-readonly SCRIPT_PATH=$(readlink "$0")
 readonly SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-echo "Attempting install to: /tmp/abin/wrap-shell.sh"
-mkdir -p "/tmp/abin"
-install -v -m755 "$SCRIPT_DIR/wrap-shell.sh" "/tmp/abin/wrap-shell.sh"
+echo "Attempting install to: /bin/wrap-shell.sh"
+install -v -m755 "$SCRIPT_DIR/wrap-shell.sh" "/bin/wrap-shell.sh"
 if [ "$?" != "0" ]; then
    echo "Failed, retrying with sudo"
-   sudo install -v -m755 "$SCRIPT_DIR/wrap-shell.sh" "/tmp/abin/wrap-shell.sh"
+   sudo install -v -m755 "$SCRIPT_DIR/wrap-shell.sh" "/bin/wrap-shell.sh"
 fi
-echo "/tmp/abin" >> $GITHUB_PATH
