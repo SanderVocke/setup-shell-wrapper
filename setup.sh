abb1efb28bf598003e73847830d313f93ed2cee7
Sander Vocke <sandervocke@gmail.com>
2025-05-06 12:35:02 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 readonly SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-if [ ! -z "$__SETUP_SHELL_WRAPPER_VERBOSE"]; then
+if [ -n "$__SETUP_SHELL_WRAPPER_VERBOSE" ]; then
   echo "setup-shell-wrapper: Attempting install to: /tmp/bin"
 fi
 
 mkdir -p /tmp/bin
 if [ "$?" != "0" ]; then
-   if [ ! -z "$__SETUP_SHELL_WRAPPER_VERBOSE"]; then
+   if [ -n "$__SETUP_SHELL_WRAPPER_VERBOSE" ]; then
      echo "setup-shell-wrapper: Failed to create /tmp/bin, retrying with sudo"
    fi
    sudo mkdir -p /tmp/bin
@@ -15,7 +15,7 @@ fi
 
 install -v -m755 "$SCRIPT_DIR/wrap-shell" "/tmp/bin/wrap-shell"
 if [ "$?" != "0" ]; then
-   if [ ! -z "$__SETUP_SHELL_WRAPPER_VERBOSE"]; then
+   if [ -n "$__SETUP_SHELL_WRAPPER_VERBOSE" ]; then
      echo "setup-shell-wrapper: Failed to install wrap-shell, retrying with sudo"
    fi
    sudo install -v -m755 "$SCRIPT_DIR/wrap-shell" "/tmp/bin/wrap-shell"
@@ -24,7 +24,7 @@ fi
 if [ ! -z "$INSTALL_BAT" ]; then
    install -v -m755 "$SCRIPT_DIR/wrap-shell.bat" "/bin/wrap-shell.bat"
    if [ "$?" != "0" ]; then
-      if [ ! -z "$__SETUP_SHELL_WRAPPER_VERBOSE"]; then
+      if [ -n "$__SETUP_SHELL_WRAPPER_VERBOSE" ]; then
         echo "setup-shell-wrapper: Failed to install wrap-shell.bat, retrying with sudo"
       fi
       sudo install -v -m755 "$SCRIPT_DIR/wrap-shell.bat" "/bin/wrap-shell.bat"
